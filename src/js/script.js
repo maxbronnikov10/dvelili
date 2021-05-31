566526636d2d20512e3b237580fb7ee710f96d28
Maxim <maxbronnikov2004@gmail.com>
2021-05-31 03:47:24 +0300

    repair errors and add new pages, animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,81 +1,81 @@
+if (document.querySelector('body').dataset.id == "main") {
+    const left = document.querySelector('.instagram__arrow-left'),
+        right = document.querySelector('.instagram__arrow-right'),
+        instagramItems = document.querySelector('.instagram__wrapper');
+
+    var index = 2;
+
+    function getClassesForSlides() {
+        for (i = 0; i < instagramItems.childNodes.length; i++) {
+
+            instagramItems.childNodes[i].className = "";
+
+            instagramItems.childNodes[i].classList.add('instagram__item');
+            if (i == index) instagramItems.childNodes[i].classList.add("instagram__item-center");
+            else if (i >= index + 3) instagramItems.childNodes[i].classList.add("instagram__item-none");
+            else if (i == index - 1 || i == index + 1) instagramItems.childNodes[i].classList.add('instagram__item-closest');
+            else instagramItems.childNodes[i].classList.add('instagram__item-another');
+        }
+    }
+
+    window.addEventListener('DOMContentLoaded', () => {
+        getClassesForSlides();
+    });
+
+    left.addEventListener('click', (e) => {
+        e.preventDefault();
+        const a = instagramItems.childNodes[instagramItems.childNodes.length - 1];
+        const b = instagramItems.childNodes[index - 2];
+        setTimeout(instagramItems.insertBefore(a, b), 1000);
+        getClassesForSlides();
+    });
+
+    right.addEventListener('click', (e) => {
+        e.preventDefault();
+        const a = instagramItems.childNodes[instagramItems.childNodes.length - 1];
+        const b = instagramItems.childNodes[index - 2];
+        setTimeout(instagramItems.insertBefore(b, a), 1000)
+        getClassesForSlides();
+    });
+}
+
 const hamburger = document.querySelector('.promo__hamburger'),
-    logo = document.querySelector('.promo__logo'),
-    social = document.querySelector('.promo__social'),
+    logo = document.querySelectorAll('.logo')[0],
+    social = document.querySelectorAll('.social')[0],
     contacts = document.querySelector('.promo__contacts'),
     cart = document.querySelector('.promo__cart'),
     favorites = document.querySelector('.promo__favorites'),
     promo = document.querySelector('.promo'),
-    menuList = document.querySelectorAll('.promo__menu-item'),
     wrapper = document.querySelector('.presentation__wrapper'),
     dropdownmenu = document.querySelector('.presentation__dropdownmenu'),
-    pageup = document.querySelector('.pageup'),
-    left = document.querySelector('.instagram__arrow-left'),
-    right = document.querySelector('.instagram__arrow-right'),
-    instagramItems = document.querySelector('.instagram__wrapper');
-
-var index = 2;
-function getClassesForSlides() {
-    for (i = 0; i < instagramItems.childNodes.length; i++) {
-
-        instagramItems.childNodes[i].className = "";
-
-        instagramItems.childNodes[i].classList.add('instagram__item');
-        if (i == index) instagramItems.childNodes[i].classList.add("instagram__item-center");
-        else if (i >= index + 3) instagramItems.childNodes[i].classList.add("instagram__item-none");
-        else if (i == index - 1 || i == index + 1) instagramItems.childNodes[i].classList.add('instagram__item-closest');
-        else instagramItems.childNodes[i].classList.add('instagram__item-another');
-    }
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    getClassesForSlides();
-});
-
-left.addEventListener('click', (e) => {
-    e.preventDefault();
-    const a = instagramItems.childNodes[instagramItems.childNodes.length - 1];
-    const b = instagramItems.childNodes[index - 2];
-    setTimeout(instagramItems.insertBefore(a, b), 1000);
-    getClassesForSlides();
-});
-
-right.addEventListener('click', (e) => {
-    e.preventDefault();
-    const a = instagramItems.childNodes[instagramItems.childNodes.length - 1];
-    const b = instagramItems.childNodes[index - 2];
-    setTimeout(instagramItems.insertBefore(b, a), 1000)
-    getClassesForSlides();
-});
-
+    pageup = document.querySelector('.pageup');
 
 hamburger.addEventListener('click', () => {
     hamburger.style.pointerEvents = 'none'
     promo.classList.toggle('promo-active');
     hamburger.classList.toggle('promo__hamburger-active');
-    logo.classList.toggle('promo__logo-active');
-    social.classList.toggle('promo__social-active');
+    logo.classList.toggle('logo-active');
+    social.classList.toggle('social-active');
     contacts.classList.toggle('promo__contacts-active');
     cart.classList.toggle('promo__cart-active');
     favorites.classList.toggle('promo__favorites-active');
-    menuList.forEach(function (e) {
-        e.classList.toggle('promo__menu-item-active');
-    });
 
-
-    if (wrapper.classList.contains('presentation__wrapper-active')) {
-        wrapper.classList.remove('presentation__wrapper-active');
-        setTimeout(function () {
-            wrapper.classList.remove('presentation__wrapper-hidden');
-        }, 20);
-    } else {
-        wrapper.classList.add('presentation__wrapper-hidden');
-        wrapper.addEventListener('transitionend', function (e) {
-            wrapper.classList.add('presentation__wrapper-active');
-        }, {
-            capture: false,
-            once: true,
-            passive: false
-        });
+    if (document.querySelector('body').dataset.id == "main") {
+        if (wrapper.classList.contains('presentation__wrapper-active')) {
+            wrapper.classList.remove('presentation__wrapper-active');
+            setTimeout(function () {
+                wrapper.classList.remove('presentation__wrapper-hidden');
+            }, 20);
+        } else {
+            wrapper.classList.add('presentation__wrapper-hidden');
+            wrapper.addEventListener('transitionend', function (e) {
+                wrapper.classList.add('presentation__wrapper-active');
+            }, {
+                capture: false,
+                once: true,
+                passive: false
+            });
+        }
     }
 
     dropdownmenu.classList.toggle("presentation__dropdownmenu-active");
